0c3c0116a68d47729a53a0d47f942f2dfc2ae521
Dmitriy <zora5891@rambler.ru>
2025-11-05 13:23:37 +0300

    so far so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.masks import get_mask_card_number, get_mask_account
-
+from src.widget import mask_account_card, get_date
 
 
 if __name__ == "__main__":
@@ -12,3 +12,8 @@ if __name__ == "__main__":
     account = 73654108430135874305
     masked_account = get_mask_account(account)
     print(f"{account} -> {masked_account}")
+
+    # Примеры маскировки данных и форматирование даты
+    print(mask_account_card("Maestro 1596837868705199"))
+    print(mask_account_card("Счет 64686473678894779589"))
+    print(get_date("2024-03-11T02:26:18.671407"))
---
+++ src/widget.py
@@ -0,0 +1,40 @@
+"""Модуль для работы с маскировкой данных и форматированием дат."""
+
+from src.masks import get_mask_card_number, get_mask_account
+
+
+def mask_account_card(input_string: str) -> str:
+    """Маскирует номер карты или счета в строке."""
+
+    # Разделяем строку на части (тип + номер)
+    parts = input_string.strip().split()
+    if len(parts) < 2:
+        raise ValueError
+
+    # Тип — элементы, кроме последнего. Номер — последний элемент
+    type_part = " ".join(parts[:-1])
+    number_str = parts[-1]
+
+    # Является ли номер числом
+    if not number_str.isdigit():
+        raise ValueError
+
+    number = int(number_str)
+
+    # Если начинается с "Счет" это счет, иначе — карта
+    if type_part.startswith("Счет"):
+        masked_number = get_mask_account(number)
+    else:
+        masked_number = get_mask_card_number(number)
+
+    return f"{type_part} {masked_number}"
+
+
+from datetime import datetime
+
+
+def get_date(date_string: str) -> str:
+
+    dt = datetime.fromisoformat(date_string)
+
+    return dt.strftime("%d.%m.%Y")
